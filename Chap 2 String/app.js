547a5eda0bf9b4270a3834e650a85ed1abbfcc33
Farhana <124885288+Dev-Farhana@users.noreply.github.com>
2023-05-05 19:25:54 +0500

    Add files via upload

---
+++ Chap 2 String/app.js
@@ -0,0 +1,41 @@
+//Question 1
+// var username;
+
+//Question 2
+//var myName = "Farhana ";
+
+//Question 3
+//var message = "Hello World";
+//alert(message);
+
+//Question 4
+// let studentName = "Hana" ; 
+// alert(studentName);
+// let studentdAge = 25 ;
+// alert(studentAge);
+// let studentSubject = "Web Development";
+// alert (studentSubject);
+
+// Question 5
+//   let dough;
+//   dough = "                              PIZZA\n                                PIZZ\n                                  PIZ\n                                     PI\n                                      P" ;
+//   alert(dough);
+
+//Question 6
+ // let email = "sol.farhanagabol2020@gmail.com" ;
+// alert ( "My email address is " + email);
+
+//Question 7
+// let book = "A smarter way to learn JavaScript" ;
+//alert( "I am trying to learn from the book"+ " " + book); 
+
+//Question 8
+//document.write( "Yah! I can write HTML content through JS")
+
+//Question 9
+//const character = " ▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬ ";
+//alert (character);
+
+
+
+
