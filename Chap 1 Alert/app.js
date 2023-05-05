469856a25248996ef13b4d75c4299c55a5405f85
Farhana <124885288+Dev-Farhana@users.noreply.github.com>
2023-05-05 19:23:59 +0500

    Add files via upload

---
+++ Chap 1 Alert/app.js
@@ -0,0 +1,25 @@
+
+//Question 1;
+//alert( "Welcome ! How is everything ? \n Long time no see!")
+
+// Question 2;
+// alert("Error! Please enter a valid password.");
+
+
+//Question 3;
+// alert("Welcome to the JS land.. \n Happy Coding!"); 
+
+
+//Question 4
+ // alert( "Welcome to JS land..");
+ // alert ( "prevent this page from creating addtional dialogs. ");
+
+//Question 5
+ // console.log(" Hello... I can run JS through my web browser's console.");
+
+// Question 6
+// alert("Your Computer has been Hacked! Enjoy :p" ) ;
+
+ // kindly check HTML file for Quetion 7 answer is there;    
+      
+
