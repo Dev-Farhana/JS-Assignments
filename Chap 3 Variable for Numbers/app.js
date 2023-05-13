8e495442658d33f11263f109145b8f868e2738a1
Farhana <124885288+Dev-Farhana@users.noreply.github.com>
2023-05-13 13:17:36 +0500

    Add files via upload

---
+++ Chap 3 Variable for Numbers/app.js
@@ -0,0 +1,19 @@
+//Q Num: 1
+//var age = 24 ;
+//alert( "I am " + age  + " years old");
+
+// Q Num:2
+// var track= 9;
+// alert( "You have visited this site " + track  + " times") ;
+
+// Q Num:3
+// let birthYear = 1997 ;
+// document.write("<h1> My birth year is " +  birthYear + "</h2> ");
+// document.writeln( "Data type of my declared variable is " + typeof (birthYear));
+
+// Q Num: 4 
+//  let visitorName= "John Doe";
+//  let product= "T-shirt(s)";
+//  let quantity= 5;
+//  document.write ( "<b>" + visitorName +  "</b>" + " ordered " + "<b>"+ quantity + "</b>" + " " + "<b>" + product+ "</b>" + " on XYZ Clothing store .")
+
