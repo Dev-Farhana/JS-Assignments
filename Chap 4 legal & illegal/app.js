61ae88a22a29519a007015972f3260a013ac491b
Farhana <124885288+Dev-Farhana@users.noreply.github.com>
2023-05-15 23:32:36 +0500

    Add files via upload

---
+++ Chap 4 legal & illegal/app.js
@@ -0,0 +1,29 @@
+    // Q NO: 1;
+    // var firstName= "Farhana" ; city= "Karachi"; institute= "SMIT";
+    // console.log(firstName, city , institue );
+
+    // Q NO: 2;
+    //  Legal names;
+    //  var firstName =" Farhana" ,  $city= "KHI", person1= "Myself", _num= 2,  age= 10 ;
+    // console.log( firstName, $city, person1,  _num, age );
+
+
+    //  illegal names;
+    //   var &person,  1num ,  @city,  let, #num!;
+    // console.log( &person,  1num,  @city,  let, #num! );
+
+
+    // Q NO: 3; Display in browser;
+   // part (a,b,c,d,e);
+
+    // document.write ("<h1> Rules for naming JS variables </h1>" );
+
+    //     document.write
+    //    ( " <p>Variable names can only contain, <i>alphabets, numbers, dollar '$' signs </i> and <i>underscores '_'</i> . For example: $my_1stVariable</p>");
+ 
+    //     document.write
+    //     ( "<p> Variable must begin with a <i>dollar '$' sign, underscore '_'</i> or <i>alphabet</i> . For example $name, _name or name ");
+     
+    //  document.write ( " <p>Variable names are case <u> sensitive</u> </p> ");
+        
+    //   document.write ("Variable names should not be JS <u> keyword </u>");
