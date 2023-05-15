0e0d6f2595fe86e1f689f73968534d73f595e199
Farhana <124885288+Dev-Farhana@users.noreply.github.com>
2023-05-15 23:35:35 +0500

    Add files via upload

---
+++ Chap 12-13/app.js
@@ -0,0 +1,116 @@
+
+    //Task: 1;
+          
+            // var character = prompt("Enter a character:");
+            // var code = character.charCodeAt(0);
+
+            //  if (code >= 48 && code <= 57) {
+            //     alert("The given input is a number.");
+            //     }
+            //      else if (code >= 65 && code <= 90) {
+            //     alert("The given input is an uppercase letter.");
+            //     }
+            //      else if (code >= 97 && code <= 122) {
+            //     alert("The given input is a lowercase letter.");
+            //     }
+            //      else {
+            //     alert("The given input is not a number, uppercase letter, or lowercase letter.");
+            //     }
+            
+
+    //Task: 2; Write a JavaScript program that accept two integers and  
+    // display the larger. Also show if the two integers are equal.
+            
+            // var num1 = prompt("Enter the first integer:");
+            // var num2 = prompt("Enter the second integer:");
+
+            //     if (num1 > num2) {
+            //     alert ("The larger number is: " + num1);
+            //     } 
+            //     else if (num2 > num1) {
+            //     alert ("The larger number is: " + num2);
+            //     } else {
+            //     alert ("Both integers are equal.");
+            //     }
+
+    //Task: 3; take input a number from user & state whether the number is positive, negative or zero.
+
+            // var number = prompt("Enter a number:");
+
+            //  if (number > 0) {
+            //       alert ("The number is positive.");
+            //     }
+            //          else if (number < 0) {
+            //         alert ("The number is negative.");
+            //         }
+            //          else {
+            //         alert ("The number is zero.");
+            //         }
+
+    //Task: 4; take a character and returns true if it is a vowel, false otherwise.
+            
+            //   var character = prompt("Enter a character:");
+            //     var isVowel = false;
+
+            //         if (
+            //         character === "a" ||
+            //         character === "e" ||
+            //         character === "i" ||
+            //         character === "o" ||
+            //         character === "u"
+            //         ) {
+            //         isVowel = true;
+            //         }
+
+            //          if (isVowel) {
+            //         alert ("The character is a vowel.");
+            //         }   
+            //             else {
+            //         alert ("The character is not a vowel.");
+            //         }
+
+
+    //Task: 5;
+            //  var correctPassword = "MyPass123";
+            //    var userPassword = prompt("Enter your password:");
+
+            //         if (!userPassword) {
+            //         alert("Please enter your password.");
+            //         } 
+            //         else if (userPassword === correctPassword) {
+            //         alert ("Correct! The password you entered matches the original password.");
+            //         } 
+            //         else {
+            //         alert ("Incorrect password.");
+            //         }
+
+    //Task: 6; This if/else statement does not work. Try to fix it:
+                    // var greeting;
+                    // var hour = 13;
+                    // if (hour < 18) {
+                    //     alert(greeting = "Good day" );
+                    // }
+                    // else { 
+                    //     alert ( greeting = "Good evening" );
+                    // }
+
+
+    //Task: 7;  Write a program that takes time as input from user in 24
+        // hours clock format like: 1900 = 7pm. Implement the
+        // following case using if, else & else if statements.
+
+        // var userTime = prompt ( "Enter you time here in 24-hour clock format ");
+               
+        //         if (userTime >= 0 && userTime <= 2359) {                
+        //         if ( userTime >=0 && userTime <= 1700) {
+        //                     alert("7pm Dinner Time ");
+        //             } 
+        //             else if (userTime  >= 1200 && userTime < 1700 ) {
+        //             alert (" Good Afternoon!");
+        //             } 
+        //             }else {
+        //             alert (" Go Study what you are doing here?");
+        //             }
+
+                    
+
