4c748b5fcf304d65e7d30a4cc320df059dcc2365
Farhana <124885288+Dev-Farhana@users.noreply.github.com>
2023-06-25 20:31:50 +0500

    Created

---
+++ chapter 26-30 Math Method/app.js
@@ -0,0 +1,31 @@
+// ------------------------------------------ Chapter 26 to 30; -----------------------------------------------------------------------------
+
+    // Q no.1; Write a program that takes a positive integer from user & display the following in your browser.
+                // a. number
+                // b. round off value of the number
+                // c. floor value of the number
+                // d. ceil value of the number
+
+                const num = 3.45214;
+                console.log("Number:",num);
+                console.log("Round off value:",Math.round(num) );
+                console.log("floor value:", Math.floor(num) );
+                console.log("ceil value:", Math.ceil(num) );
+
+    // 2. Write a program that takes a negative floating point number from user & display the following in your browser.
+                // a. number
+                // b. round off value of the number
+                // c. floor value of the number
+                // d. ceil value of the number
+
+                const num2 = -2.673;
+                console.log("Number:",num2);
+                console.log("Round off value:", Math.round(num2));
+                console.log("Floor value:",Math.floor(num2));
+                console.log("Ceil value:", Math.ceil(num2));
+
+    // 3. Write a program that displays the absolute value of a number. E.g. absolute value of -4 is 4 & absolute value of 5 is 5
+
+                console.log("Absolute value of", -4, "is", Math.abs(-4));
+
+
